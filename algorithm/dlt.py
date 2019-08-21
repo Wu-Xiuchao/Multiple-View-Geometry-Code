1052a8592541b9073abfb08bed99c7776c208b0a
Wu-Xiuchao <383137597@qq.com>
2019-08-21 12:42:11 +0800

    u

--- algorithm/dlt.py
+++ algorithm/dlt.py
@@ -22,13 +22,20 @@ def basic_dlt(point_list1,point_list2):
 		else:
 			A = np.vstack([A,Ai])
 
-	u,s,vt = np.linalg.svd(A,full_matrices=0,compute_uv=1) # 这里 s 从大到小排列
+	u,s,vt = np.linalg.svd(A,full_matrices=1,compute_uv=1) # 这里 s 从大到小排列
+
 	h = vt[-1].reshape((3,3))
 	
 	return h
 
-
-def normilized_dlt(point_list1,point_list2,debug=False):
+"""Normilized DLT algorithm
+para:
+	point_list1: [X1,X2,X3,X4,...Xn] (point_list1 * H = point_list2)
+	for each X: [x1,y1,w1]
+return:
+	homography
+"""
+def normilized_dlt(point_list1,point_list2):
 
 	def normilize(data):
 		s = math.sqrt(2) * (1/np.mean(np.linalg.norm(data[:,:2]-np.mean(data[:,:2],axis=0),axis=1)))
@@ -41,18 +48,7 @@ def normilized_dlt(point_list1,point_list2,debug=False):
 		return data,T
 
 	point_list1,T = normilize(point_list1)
-	if debug is True:
-		print('T:',T)
-		print('centroid:',np.mean(point_list1,axis=0))
-		print('mean_distance:',np.mean(np.linalg.norm(point_list1[:,:2],axis=1)))
-		print(point_list1)
-
 	point_list2,T_ = normilize(point_list2)
-	if debug is True:
-		print('T\':',T_)
-		print('centroid:',np.mean(point_list2,axis=0))
-		print('mean_distance:',np.mean(np.linalg.norm(point_list2[:,:2],axis=1)))
-		print(point_list2)
 
 	tilde_h = basic_dlt(point_list1,point_list2)
 
--- test.py
+++ test.py
@@ -1,20 +1,19 @@
 import numpy as np
 from algorithm import dlt
-import cv2
+import cv2,sys
 
 
-point_list1 = np.array([[141,131,1],[480,159,1],[493,630,1],[64,601,1]])
+point_list1 = np.array([[10,100,1],[281,37,1],[499,341,1],[93,492,1]])
 
-point_list2 = np.array([[318,256,1],[534,372,1],[316,670,1],[73,473,1]])
+point_list2 = np.array([[62,55,1],[424,54,1],[448,564,1],[53,573,1]])
 
-# print('centroid:',np.mean(point_list1,axis=0))
-# print('mean_distance:',np.mean(np.linalg.norm(point_list1[:,:2],axis=1)))
+
 h1 = dlt.basic_dlt(point_list1,point_list2)
 print(h1)
 print(np.linalg.norm(h1))
 
 
-h2 = dlt.normilized_dlt(point_list1,point_list2,False)
+h2 = dlt.normilized_dlt(point_list1,point_list2)
 print(h2)
 print(np.linalg.norm(h2))
 
@@ -22,3 +21,13 @@ print(np.linalg.norm(h2))
 h,_ = cv2.findHomography(point_list1[:,:2],point_list2[:,:2])
 print(h)
 print(np.linalg.norm(h))
+
+
+pic = cv2.imread(sys.argv[1])
+
+img = cv2.warpPerspective(pic,h2,(pic.shape[1],pic.shape[0]))
+cv2.imwrite('res.png',img)
+# pic = cv2.imread(sys.argv[1])
+# cv2.imshow('pic',pic)
+
+# cv2.waitKey(0)
